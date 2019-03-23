c9ca1603efbe8258ee203eced2aa147961db5a55
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-23 17:43:56 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,2 +1,28 @@
+def division(num1, num2)
+  num1 / num2
+end
 
+def assign_variable(value)
+  value = "Bob"
+end
+
+def argue(phrase)
+  phrase
+end
+
+def greeting(greeting, name)
+
+end
+
+def return_a_value(phrase = "Nice")
+  phrase
+end
+
+def last_evaluated_value(phrase = "expert")
+  phrase
+end
+
+def pizza_party(taste = "cheese")
+  taste
+end
 
